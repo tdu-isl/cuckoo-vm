0751bef85263147c9835a2735d42cec9f985da6a
howmuch515 <another.face0515@gmail.com>
2019-09-14 13:19:11 +0900

    :sparkles: auto-login & remote-rpc

--- setup_win7.ps1
+++ setup_win7.ps1
@@ -19,6 +19,13 @@ python -m pip install pillow
 echo "[+] Disable UAC"
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
 
+# Enable Auto-login & Remote-RPC
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultUserName /d /t REG_SZ /f
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultPassword /d /t REG_SZ /f
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v AutoAdminLogon /d 1 /t REG_SZ /f
+reg add "hklm\system\CurrentControlSet\Control\TerminalServer" /v AllowRemoteRPC /d 0x01 /t REG_DWORD /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /d 0x01 /t REG_DWORD /f
+
 # Change Default GW
 echo "[+] Change IP ==> 192.168.56.1 mask 255.255.255.0 gw 192.168.56.1"
 netsh interface ipv4 set address "Local Area Connection" static 192.168.56.101 255.255.255.0 192.168.56.1
