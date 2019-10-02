a36d3756dda590958a18bee6973b4306ab44f921
howmuch515 <another.face0515@gmail.com>
2019-10-02 15:53:16 +0900

    :bug: スクリプト途中でパスワード要求される問題を解決

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo apt-get update -y
 # Install Cuckoo
 sh install_cuckoo.sh
 
-# Setup Cuckoo
-sh setup_cuckoo.sh
-
 # Install log analysis tools
 sh install_log_analysis_tools.sh
+
+# Setup Cuckoo
+sh setup_cuckoo.sh
