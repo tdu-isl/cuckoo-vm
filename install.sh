a7495e39cf3814e34c439c6983e7d20acaf19927
howmuch515 <another.face0515@gmail.com>
2019-10-11 16:51:39 +0900

    :wrench: LOG file

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+. module/log.sh
+LOG_FILE="/var/log/cuckoo_vm.log"
 
 # Package Update
-sudo apt-get update -y
+sudo apt-get update -y >> $LOG_FILE
 
 # Install Cuckoo
-sh scripts/install_cuckoo.sh
+sh scripts/install_cuckoo.sh >> $LOG_FILE
 
 # Install log analysis tools
-sh scripts/install_log_analysis_tools.sh
+sh scripts/install_log_analysis_tools.sh >> $LOG_FILE
 
 # Setup Cuckoo
-sh scripts/setup_cuckoo.sh
+sh scripts/setup_cuckoo.sh >> $LOG_FILE
 
 # Show banner
 cat ./BANNER.txt
